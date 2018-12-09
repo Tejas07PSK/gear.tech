f9eeb1b7c18e3d5318c2da3ee280b61781b1e10d
Palash Sarkar <palashsarkar0007@gmail.com>
2018-12-10 04:27:36 +0530

    Modified connection manager!!

--- models/DbConnectManage.js
+++ models/DbConnectManage.js
@@ -47,13 +47,13 @@ async function stop()
 
 }
 
-start();
+(async () => { await start() })();
 
 mongo.connection.on('reconnect', function () { console.log("Mongoose reconnected to : " + url); });
 mongo.connection.on('connected', function () { console.log("Mongoose default connection open to : " + url + "............."); console.log("Connection Successful!!"); });
 mongo.connection.on('disconnected', function () { console.log("Mongoose default connection closed to : " + url); console.log("Disconnected!!"); });
 mongo.connection.on('reconnectFailed', function () { console.log("Mongoose failed to reconnect to : " + url); console.log("Ran out of retries."); });
 //mongo.connection.on('error', function (err) { console.log("Mongoose encountered an ERROR while connecting : default mode!!"); console.log(err); });
-process.on('SIGINT', function () { stop(); console.log('Mongoose default connection disconnected through app termination!!'); process.exit(0); });
+process.on('SIGINT', async function () { await stop(); console.log('Mongoose default connection disconnected through app termination!!'); process.exit(0); });
 
 module.exports = { "open" : start , "close" : stop, "getDb" : function () { return (db); } };
