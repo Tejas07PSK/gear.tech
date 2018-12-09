cfbf4767ff014d6c67c26a94d4afe0f06c1796d4
Palash Sarkar <palashsarkar0007@gmail.com>
2018-12-10 04:17:13 +0530

    Added 'mongoose' schema for MongoDb collection 'Schemas.js'!!

---
+++ models/Schemas.js
@@ -0,0 +1,26 @@
+/*
+
+        @Author : Tejas07PSK (Palash Sarkar),
+        @CreatedON : 10 Dec, 2018, 3:49 AM,
+        @File-Name : Schemas.js
+
+ */
+
+"use strict";
+
+const Schema = (require("mongoose")).Schema;
+
+module.export.coll1 = new Schema({
+
+    "_id" : Schema.Types.ObjectId,
+    "key_id" : { 'type' : String, 'unique' : true, 'required' : true },
+    "message" : { 'type' : String, 'unique' : true, 'required' : true },
+    "md" : {
+
+        "hex" : { 'type' : String, 'unique' : true, 'required' : true },
+        "base64" : { 'type' : String, 'unique' : true, 'required' : true }
+
+    },
+    "dateAdded" : { 'type' : Date, 'default' : Date.now() }
+
+});
