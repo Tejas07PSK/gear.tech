d3745c828f8d7be59adef342bfbf0274000a8b0c
Palash Sarkar <palashsarkar0007@gmail.com>
2018-12-10 22:23:25 +0530

    Modified 'crud.js' for db crud ops !!

--- models/crud.js
+++ models/crud.js
@@ -8,19 +8,29 @@
 
 const models = require("./schemasandmodels");
 
+let eff = undefined;
 module.export = {
 
     "createDoc" : async function(obj) {
 
-        let eff = undefined;
         await ((new models.Dashboard(obj)).save()).then(
 
-            () => { console.log("Document insertion successful in collection \'Dashboard\'!!"); eff = obj.key_id; },
-            (err) => { console.log("Document insertion failed!! ERROR!!"); console.log(err); eff = false; }
+                () => { console.log("Document insertion successful in collection \'Dashboard\'!!"); eff = obj.key_id; },
+                (err) => { console.log("Document insertion failed!! ERROR!!"); console.log(err); eff = false; }
 
             );
         return (eff);
 
-    }
+    },
+    "getUrlFromKeyId" : async function(key_id) {
+
+       await (models.Dashboard).findOne({ 'key_id' : key_id }, 'message', function (err, doc) {
+
+           if(err) { console.log("Document retrieval failed!! Error!!"); console.log(err); eff = false; }
+           else { console.log("Document retrieval successful!!"); eff = doc.message; }
+       });
+       return (eff);
+
+    },
 
 };
