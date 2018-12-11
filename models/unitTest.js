c580719d03b0a10e43f2717c7791e2df837b3afa
Palash Sarkar <palashsarkar0007@gmail.com>
2018-12-12 04:50:23 +0530

    Modified 'unitTest.js' for 'models' module !!

--- models/unitTest.js
+++ models/unitTest.js
@@ -10,7 +10,6 @@
 
 const conn = require("./dbconnectmanage");
 const ops = require("./crud");
-
 const test_obj = {
 
     'key_id' : "qs223ery",
@@ -19,12 +18,17 @@ const test_obj = {
     'dateAdded' : Date.now()
 
 };
+let inpKeyId = "qs223ery", inpURL = "gog.com", updtKeyId = "gst32";
 
 (async () => { await conn.open() })();
 
-let res = undefined;
+(ops.createDoc(test_obj)).then((res) => { if (res !== false) { console.log(`Shortened URL Id : ${res}`); } else { console.log("URL not shortened!!"); } });
 
-res = ops.createDoc(test_obj);
-//res = ops.deleteDocByKeyId("qs223ery");
+(ops.getUrlFromKeyId("qs223ery")).then((res) => { if (res !== false) { console.log(`Original URL is : ${res}`); } else { console.log("Invalid 'key_id', URL not found!!"); } });
 
-if (res !== false) { console.log(`Shortened URL Id : ${res}`); } else { console.log("URL not shortened!!"); }
+(ops.getDocFromUrl("gog.com")).then((res) => { if (res !== false) { console.log("URL already exists!!"); } else { console.log("URL does not exist!!"); } });
+
+(ops.updateDocKeyId("qs223ery", "gst32")).then((res) => { console.log("Update promise resolved!!"); console.log(`Status : ${res}`); });
+
+(ops.deleteDocByKeyId("gst32")).then((res) => { console.log("Deletion promise resolved!!"); console.log(`Status : ${res}`); });
+
