e2c4ce8773806baa261b47ad8c61bd68ed3affab
Palash Sarkar <palashsarkar0007@gmail.com>
2018-12-12 22:31:48 +0530

    Modified 'unitTest.js' of 'models' module !!

--- models/unitTest.js
+++ models/unitTest.js
@@ -22,7 +22,7 @@ let inpKeyId = "qs223ery", inpURL = "gog.com", updtKeyId = "gst32";
 
 (async () => { await conn.open() })();
 
-(ops.createDoc(null)).then((res) => { if (res !== false) { console.log(`Shortened URL Id : ${res}`); } else { console.log("URL not shortened!!"); } });
+(ops.createDoc(true)).then((res) => { if (res !== false) { console.log(`Shortened URL Id : ${res}`); } else { console.log("URL not shortened!!"); } });
 
 //(ops.getUrlFromKeyId(inpKeyId)).then((res) => { console.log("Input \'key_id\' : " + inpKeyId); if (res !== false) { console.log(`Original URL is : ${res}`); } else { console.log("Invalid \'key_id\', URL not found!!"); } });
 
