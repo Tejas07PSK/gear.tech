15c2b375b82de23f07189387bc8f200c557c91ab
Palash Sarkar <palashsarkar0007@gmail.com>
2018-12-15 14:31:16 +0530

    Added 'resobj.js' utility script to  create result 'JSON' obj !!

---
+++ resobj.js
@@ -0,0 +1,15 @@
+/*
+
+        @Author : Tejas07PSK (Palash Sarkar),
+        @CreatedON : 15 Dec, 2018, 2:18 PM,
+        @File-Name : resobj.js
+
+ */
+
+module.exports.ResObj = function ( ...n ) {
+
+    this.stat = n[0];
+    this.msg = n[1];
+    this.res = n[2];
+
+};
