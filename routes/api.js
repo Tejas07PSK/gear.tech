81813d640da4a7588925fa18e70b606bd6b2c52c
Palash Sarkar <palashsarkar0007@gmail.com>
2018-12-16 22:38:23 +0530

    Modified 'api.js' route !!

--- routes/api.js
+++ routes/api.js
@@ -6,10 +6,10 @@
 
  */
 
-const express = require('express');
 const crud = require('../models/crud');
 const ro = require('../resobj');
-const router = express.Router();
+const genId = require('../hashing_id/generate-id');
+const router = (require('express')).Router();
 
 router.get('/', function (req, res, next) {
 
@@ -47,11 +47,11 @@ router.get('/', function (req, res, next) {
             }
             else {
 
-                console.log("Either an invalid 'url_id' has been passed or some internal error has occured. URL not found !!");
+                console.log("Either an invalid \'url_id\' has been passed or some internal error has occurred. URL not found !!");
                 res.status(404);
                 res.end(
 
-                    JSON.stringify(new ro.ResObj("0", "Either an invalid \'url_id\' has been passed or some internal error has occured. No URL found !! (http - 404)", "")),
+                    JSON.stringify(new ro.ResObj("0", "Either an invalid \'url_id\' has been passed or some internal error has occurred. No URL found !! (http - 404)", "")),
                     "utf-8", function () { console.log("Http conversation ended successfully !!"); }
 
                 );
@@ -81,18 +81,19 @@ router.post('/', function (req, res, next) {
         return;
 
     }
-    (crud.getUrlFromKeyId(url_id)).then(
+    (crud.getDocFromUrl(url)).then(
 
         (tmp) => {
 
-            console.log("Input \'key_id\' : " + url_id);
+            console.log("Input \'URL\' : " + url);
             if (tmp !== false) {
 
-                console.log(`Original URL is : ${tmp}`);
+                console.log("Shortened-id of the requested url already exists !!");
+                console.log(`\'key_id\' of the URL is : ${tmp}`);
                 res.status(200);
                 res.end(
 
-                    JSON.stringify(new ro.ResObj("1", "Valid \'url_id\'. URL retrieved successfully !! (http - 200)", tmp)),
+                    JSON.stringify(new ro.ResObj("1", "Valid \'URL\', but it was already shortened before. URL \'key_id\' successfully found !! (http - 200)", tmp)),
                     "utf-8", function () { console.log("Http conversation ended successfully !!"); }
 
                 );
@@ -100,14 +101,40 @@ router.post('/', function (req, res, next) {
             }
             else {
 
-                console.log("Either an invalid 'url_id' has been passed or some internal error has occured. URL not found !!");
-                res.status(404);
-                res.end(
+                console.log("Either an invalid \'URL\' has been passed or some internal error has occurred, or it might not exist !!");
+                (crud.createDoc(genId.formId(url))).then(
 
-                    JSON.stringify(new ro.ResObj("0", "Either an invalid \'url_id\' has been passed or some internal error has occured. No URL found !! (http - 404)", "")),
-                    "utf-8", function () { console.log("Http conversation ended successfully !!"); }
+                    (val) => {
+
+                        if (val !== false) {
+
+                            console.log(`Shortened \'key_id\' for the given URL : ${val}`);
+                            res.status(200);
+                            res.end(
+
+                                JSON.stringify(new ro.ResObj("1", "Valid \'URL\'. URL \'key_id\' successfully generated !! (http - 200)", val)),
+                                "utf-8", function () { console.log("Http conversation ended successfully !!"); }
+
+                            );
+
+                        }
+                        else {
+
+                            console.log("Either an invalid \'URL\' has been passed or some internal error has occured. URL not shortened !!");
+                            res.status(500);
+                            res.end(
+
+                                JSON.stringify(new ro.ResObj("0", "Either an invalid \'URL\' has been passed or some internal error has occurred. URL not shortened !! (http - 500)", "")),
+                                "utf-8", function () { console.log("Http conversation ended successfully !!"); }
+
+                            );
+
+                        }
+
+                    }
 
                 );
+
 
             }
 
