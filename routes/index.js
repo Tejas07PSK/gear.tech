1589da11f482ab122d732f12b173552ae1d1ca63
Palash Sarkar <palashsarkar0007@gmail.com>
2018-12-18 23:17:02 +0530

    Modified 'index.js' route !!

--- routes/index.js
+++ routes/index.js
@@ -10,13 +10,62 @@
 
 const crud = require('../models/crud');
 const router = (require('express')).Router();
-const createError = require('http-errors');
 
 /* GET home page. */
 router.get('/', function(req, res) {
 
     res.status(200);
     res.render('index', { title: 'Express' });
+
+});
+
+router.post('/', function (req, res) {
+
+    let url = req.body['url'];
+    (crud.getDocFromUrl(url)).then(
+
+        (tmp) => {
+
+            console.log("Input \'URL\' : " + url);
+            if (tmp !== false) {
+
+                console.log("Shortened-id of the requested url already exists !!");
+                console.log(`\'key_id\' of the URL is : ${tmp}`);
+                res.status(200);
+                res.end(tmp, "utf-8", function () { console.log("Http conversation ended successfully !!"); });
+
+            }
+            else {
+
+                console.log("Either an invalid \'URL\' has been passed or some internal error has occurred, or it might not exist !!");
+                (crud.createDoc(genId.formId(url))).then(
+
+                    (val) => {
+
+                        if (val !== false) {
+
+                            console.log(`Shortened \'key_id\' for the given URL : ${val}`);
+                            res.status(200);
+                            res.end(val, "utf-8", function () { console.log("Http conversation ended successfully !!"); });
+
+                        }
+                        else {
+
+                            console.log("Either an invalid \'URL\' has been passed or some internal error has occured. URL not shortened !!");
+                            res.status(500);
+                            res.end("Internal Error !! (http - 500)", "utf-8", function () { console.log("Http conversation ended successfully !!"); });
+
+                        }
+
+                    }
+
+                );
+
+            }
+
+        }
+
+    );
 
 });
 
