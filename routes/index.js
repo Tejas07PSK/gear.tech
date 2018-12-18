e635395c7a728aa0d00a048854f39d6baac02143
Palash Sarkar <palashsarkar0007@gmail.com>
2018-12-18 22:32:42 +0530

    Modified 'index.js' route !!

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,15 @@
-var express = require('express');
-var router = express.Router();
+/*
+
+        @Author : Tejas07PSK (Palash Sarkar),
+        @CreatedON : 18 Dec, 2018, 09:46 PM,
+        @File-Name : index.js
+
+ */
+
+"use strict";
+
+const crud = require('../models/crud');
+const router = (require('express')).Router();
 const createError = require('http-errors');
 
 /* GET home page. */
