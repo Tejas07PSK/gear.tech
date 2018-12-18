bec37a275fae0b53c1c42ea4abb7569bb74463cc
Palash Sarkar <palashsarkar0007@gmail.com>
2018-12-19 02:33:27 +0530

    Modified 'app.js' !!

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const ro = require('./resobj');
 const indexRouter = require('./routes/index');
-//const usersRouter = require('./routes/users');
 const apiRouter = require('./routes/api');
 const redirect = require('./routes/redirect');
 
@@ -229,7 +228,6 @@ app.use('/', function (req, res, next) {
 
 });
 app.use('/', indexRouter);
-//app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
