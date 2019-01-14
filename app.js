da8c3aff405defe8666e2b97885e10fd1300fab5
Palash Sarkar <palashsarkar0007@gmail.com>
2019-01-14 21:58:49 +0530

    Updated 'app.js' script !!

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ const redirect = require('./routes/redirect');
 const app = express();
 
 // set-up view engines
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -234,16 +235,20 @@ app.use('/', function (req, res, next) {
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
+
 app.use(function (req, res, next) { next(createError(404)); });
 
 // error handler
+
 app.use(function (err, req, res) {
 
     // set locals, only providing error in development
+
     res.locals.message = err.message;
     res.locals.error = ((req.app.get('env') === 'development') ? err : {});
 
     // render the error page
+    
     res.status(err.status || 500);
     res.render('error');
 
